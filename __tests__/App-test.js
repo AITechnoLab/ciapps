4766bef4f69ee16221e6333c9ba57b8956be0924
AITechnoLab <58978189+AITechnoLab@users.noreply.github.com>
2019-12-20 20:19:05 +0530

    Update App-test.js

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -6,6 +6,9 @@ import 'react-native';
 import React from 'react';	
 import App from '../App';	
 
+global.fetch = jest.fn(() => new Promise(resolve => resolve()));
+jest.mock('react-native-gesture-handler', () => {});
+
 // Note: test renderer must be required after react-native.	
 import renderer from 'react-test-renderer';	
 
