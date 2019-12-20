04a41caad343669f84953d63d9cab15f813f5108
AITechnoLab <58978189+AITechnoLab@users.noreply.github.com>
2019-12-20 20:06:28 +0530

    Update App-test.js

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,44 +1,14 @@
-import ReactNative from 'react-native';
-// Note: test renderer must be required after react-native.
-import {shallow} from 'enzyme';
-import renderer from 'react-test-renderer';
+/**	
+ * @format	
+ */	
 
-import React from 'react';
-import App from '../App';
+import 'react-native';	
+import React from 'react';	
+import App from '../App';	
 
-const {View, Text, Switch} = ReactNative;
+// Note: test renderer must be required after react-native.	
+import renderer from 'react-test-renderer';	
 
-describe('jest snapshot tests', () => {
-  it('renders correctly', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
-
-describe('enzyme tests', () => {
-  it('should render a <App /> component', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(View)).toHaveLength(5);
-    expect(wrapper.find(Text)).toHaveLength(9);
-    expect(wrapper.find(Switch)).toHaveLength(1);
-  });
-
-  it('should togggle switch to true', () => {
-    const wrapper = shallow(<App />);
-    const switchValueBeforeToggle = wrapper
-      .find(Switch)
-      .first()
-      .props().value;
-    expect(switchValueBeforeToggle).toBe(false);
-    wrapper
-      .find(Switch)
-      .first()
-      .props()
-      .onChange();
-    const switchValueAfterToggle = wrapper
-      .find(Switch)
-      .first()
-      .props().value;
-    expect(switchValueAfterToggle).toBe(true);
-  });
-});
+it('renders correctly', () => {	
+  renderer.create(<App />);	
+})
